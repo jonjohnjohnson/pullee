ba65a4a8ffc9df999d8ae6d182a1bb4f321ba259
jonjohnjohnson <hi@jonjohnjohnson.com>
2013-06-21 21:01:26 -0400

    [minor] restructure approach

--- pullee.js
+++ pullee.js
@@ -1,32 +1,41 @@
-function Pullee(node, direction, threshold, execute) {
+function Pullee() {
   self = this;
 
-  self.init = function() {
-    alert('derp');
+  self.init = function(node, axis, threshold, thresholdUnit, execute) {
+    //console.log('init()');
+    if (!execute) {
+      throw new Error('All parameters are required');
+    }
+    
+    document.body.addEventListener('touchstart', function (e) {
+      //console.log('touchstart');
+      onStart();
+    })
+    
+    document.body.addEventListener('touchmove', function (e) {
+      //console.log('touchmove');
+      onMove();
+    })
+    
+    document.body.addEventListener('touchend', function (e) {
+      //console.log('touchend');
+      onEnd();
+    })
+
   };
-  
-  document.body.addEventListener('touchstart', function (e) {
-    onStart();
-  })
-  
-  document.body.addEventListener('touchmove', function (e) {
-    e.preventDefault();
-    onMove();
-  })
-  
-  document.body.addEventListener('touchend', function (e) {
-    onEnd();
-  })
-  
+
   function onStart (e) {
+    //console.log('onStart');
     
   };
   
   function onMove (e) {
-    
+    //console.log('onMove');
+    e.preventDefault();
   };
   
   function onEnd (e) {
+    //console.log('onEnd');
     
   };
 
