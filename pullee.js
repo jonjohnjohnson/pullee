258d8a30af55e6cc38d4765961f8284964ac9f9b
jonjohnjohnson <hi@jonjohnjohnson.com>
2013-06-21 22:02:13 -0400

    [major] build out error handling for all parameters

--- pullee.js
+++ pullee.js
@@ -1,10 +1,50 @@
 function Pullee() {
-  self = this;
+  var self = this;
+  var startPosition = 0;
+  var axisPoint;
 
   self.init = function(node, axis, threshold, thresholdUnit, execute) {
     //console.log('init()');
-    if (!execute) {
-      throw new Error('All parameters are required');
+    console.log();
+    var error = false;
+    var errorMessages = [];
+    
+    if (execute === undefined) {
+      throw new Error('all parameteres are required');
+    }
+    
+    if (!(node instanceof HTMLElement)) {
+      error = true;
+      errorMessages.push('\'node\' must be an HTML element');
+    }
+    
+    if (axis === 'x') {
+      axisPoint = 'pageX';
+    } else if (axis === 'y') {
+      axisPoint = 'pageY';
+    } else {
+      error = true;
+      errorMessages.push('axis must be \'x\' or \'y\'');
+    }
+    
+    if (typeof threshold !== "number") {
+      error = true;
+      errorMessages.push('threshold must be a number');
+    }
+    
+    if (thresholdUnit !== 'px' && thresholdUnit !== '%') {
+      error = true;
+      errorMessages.push('thresholdUnit must be a \'px\' or \'%\'');
+    }
+    
+    if (typeof execute !== 'function') {
+      error = true;
+      errorMessages.push('you must pass in a function to execute');
+    }
+    
+    if (error) {
+      var errorMessage = errorMessages.length + ' errors: ' + errorMessages.join(', ');
+      throw new Error(errorMessage);
     }
     
     document.body.addEventListener('touchstart', function (e) {
